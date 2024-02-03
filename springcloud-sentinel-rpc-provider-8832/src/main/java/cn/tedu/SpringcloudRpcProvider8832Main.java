693dae9813cfca8f8a35a2f2112afef0b0a56bd0
zhangb <zhangbowisky@126.com>
2024-02-03 13:54:16 +0800

    测试提交

--- springcloud-sentinel-rpc-provider-8832/src/main/java/cn/tedu/SpringcloudRpcProvider8832Main.java
+++ springcloud-sentinel-rpc-provider-8832/src/main/java/cn/tedu/SpringcloudRpcProvider8832Main.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class SpringcloudRpcProvider8832Main {
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudRpcProvider8832Main.class);
+        System.out.println("aaaa");
     }
 }
